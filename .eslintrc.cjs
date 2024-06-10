5331eb55c6a628731cc2275c3e3d94e74f34ba72
Sara Gylden <saragbusch@live.dk>
2024-06-10 11:33:48 +0200

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,22 +2,20 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
- root: true,
- extends: [
-   "plugin:vue/vue3-essential",
-   "eslint:recommended",
-   "@vue/eslint-config-prettier/skip-formatting",
- ],
- overrides: [
-   {
-     files: [
-       "cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}",
-       "cypress/support/**/*.{js,ts,jsx,tsx}",
-     ],
-     extends: ["plugin:cypress/recommended"],
-   },
- ],
- parserOptions: {
-   ecmaVersion: "latest",
- },
+  root: true,
+  env: {
+    node: true
+  },
+  globals: {
+    module: true
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/prettier",
+    "@vue/typescript"
+  ],
+  parserOptions: {
+    parser: "@typescript-eslint/parser"
+  }
 };
