af19305b4898fde9c787034a34abbba34fa4bda2
Sara Gylden <saragbusch@live.dk>
2024-06-10 12:59:52 +0200

    Update ToDoItem.cy.js

--- cypress/e2e/ToDoItem.cy.js
+++ cypress/e2e/ToDoItem.cy.js
@@ -4,14 +4,14 @@ describe('ToDoItem Component', () => {
     })
   
     it('should display a ToDoItem with label and checkbox', () => {
-      cy.get('.stack-small').should('exist') // Tjekker om ToDoItem-containeren findes
-      cy.get('.custom-checkbox').should('exist') // Tjekker om checkbox-containeren findes
-      cy.get('.checkbox-label').should('be.visible') // Vent på, at labelen er synlig
-        .and('not.be.empty') // Vent på, at labelen ikke er tom
+      cy.get('.stack-small').should('exist') 
+      cy.get('.custom-checkbox').should('exist') 
+      cy.get('.checkbox-label').should('be.visible') 
+        .and('not.be.empty') 
     })
   
     it('should allow deleting ToDoItem', () => {
-      cy.get('.btn.btn__danger').contains('Delete').click() // Simulerer klik på 'Delete' knappen
+      cy.get('.btn.btn__danger').contains('Delete').click() 
     })
   })
   
